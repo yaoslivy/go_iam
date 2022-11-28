13611e936713c0c4e243d563ff7003fd47edbfd3
yaoshuilin <slivy00@icloud.com>
2022-11-28 19:51:14 +0800

    feat: add public use packages

---
+++ pkg/app/doc.go
@@ -0,0 +1,8 @@
+// Package app defines an application framework used to create application.
+package app
+
+// app.go : implementation of the application
+// cmd.go : implementation of the command line procedures
+// config.go : implementation of the command line parameter parsing
+// flag.go : implementation of the configuration file parsing
+// options.go : implementation of the commang line options
---
+++ pkg/cli/genericclioptions/doc.go
@@ -0,0 +1,3 @@
+// Package genericclioptions contains flags which can be added to you command, bound, completed, and produce
+// useful helper functions.
+package genericclioptions
